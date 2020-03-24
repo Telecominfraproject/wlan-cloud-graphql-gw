75befca4d465fdf77eb12cb4ad5faa713e06976e
Sean Macfarlane <sean.g.macfarlane@gmail.com>
2020-03-24 13:35:56 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
